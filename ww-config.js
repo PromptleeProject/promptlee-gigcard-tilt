5a0fdccbfed353d643543684e3a7961dc1ec7a38
Promptlee <promtlee.project@gmail.com>
2025-07-24 03:37:49 +0200

    fix: add bindable: true to all props

--- ww-config.js
+++ ww-config.js
@@ -8,38 +8,47 @@ export default {
     imageUrl: {
       label: { en: "Image URL" },
       type: "Text",
+      bindable: true,
     },
     category: {
       label: { en: "Category" },
       type: "Text",
+      bindable: true,
     },
     title: {
       label: { en: "Title" },
       type: "Text",
+      bindable: true,
     },
     description: {
       label: { en: "Description" },
       type: "Text",
+      bindable: true,
     },
     price: {
       label: { en: "Price" },
       type: "Text",
+      bindable: true,
     },
     deliveryTime: {
       label: { en: "Delivery Time (h)" },
       type: "Text",
+      bindable: true,
     },
     ratingValue: {
       label: { en: "Rating Value" },
       type: "Text",
+      bindable: true,
     },
     reviewsCount: {
       label: { en: "Reviews Count" },
       type: "Text",
+      bindable: true,
     },
     gigLink: {
       label: { en: "Link to Gig" },
       type: "Link",
+      bindable: true,
     },
   },
 };
